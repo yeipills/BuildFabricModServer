ebdf45b6aa95344eb17ef6f941d4d4717feaedb2
yeipi <73313280+yeipills@users.noreply.github.com>
2023-10-08 20:14:33 -0300

    Update script_optimizacion_debian_11_ARM.sh

--- script_optimizacion_debian_11_ARM.sh
+++ script_optimizacion_debian_11_ARM.sh
@@ -9,7 +9,6 @@ sudo apt update -y && sudo apt upgrade -y
 # Instalar y configurar sysstat para monitorear el rendimiento del sistema
 echo "Instalando y configurando sysstat..."
 sudo apt install -y sysstat
-# Asegurándonos de que el archivo de configuración existe antes de intentar modificarlo
 if [ -f /etc/default/sysstat ]; then
     sudo sed -i 's/ENABLED="false"/ENABLED="true"/g' /etc/default/sysstat
 else
@@ -18,14 +17,12 @@ fi
 
 # Habilitar el uso de cache de la memoria RAM para acelerar la lectura de disco
 echo "Habilitando cache de RAM..."
-# Asegurándonos de que las configuraciones no se duplican
 if ! grep -q "vm.vfs_cache_pressure=50" /etc/sysctl.conf; then
     echo "vm.vfs_cache_pressure=50" | sudo tee -a /etc/sysctl.conf
 fi
 
 # Configurar el kernel para gestionar mejor la carga de red pesada
 echo "Optimizando la gestión de la red..."
-# Asegurándonos de que las configuraciones no se duplican
 NET_CONFIGS=("net.core.rmem_max=12582912" "net.core.wmem_max=12582912" "net.ipv4.tcp_rmem=4096 87380 12582912" "net.ipv4.tcp_wmem=4096 87380 12582912" "net.ipv4.tcp_congestion_control=cubic")
 for config in "${NET_CONFIGS[@]}"; do
     if ! grep -q "$config" /etc/sysctl.conf; then
@@ -35,9 +32,15 @@ done
 
 # Reducir el uso de SWAP para mejorar el rendimiento general
 echo "Reduciendo el uso de SWAP..."
-# Asegurándonos de que las configuraciones no se duplican
 if ! grep -q "vm.swappiness=10" /etc/sysctl.conf; then
     echo "vm.swappiness=10" | sudo tee -a /etc/sysctl.conf
+fi
+
+# Aplicar optimizaciones específicas para ARM si es aplicable
+if uname -m | grep -q "arm"; then
+    echo "Aplicando optimizaciones específicas para ARM..."
+    # Aquí puedes agregar cualquier configuración o instalación de paquete específica para ARM
+    # Por ejemplo, instalar bibliotecas optimizadas, ajustar parámetros del kernel, etc.
 fi
 
 # Reiniciar sysctl para aplicar las nuevas configuraciones
